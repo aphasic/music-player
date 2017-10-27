2cc37a698ef4e4215df5f6c47877a9294a98d21f
aphasic <13468818308@163.com>
2017-10-27 19:13:57 +0800

    更新【readme】

---
+++ src/base/songlist/songlist.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <ul class="song-list">
+      <li class="list-item" v-for="item in songlist">
+        <h2 class="title ellipse-1-line">{{item.name}}</h2>
+        <p class="desc ellipse-1-line">
+          <a class="singer" href="" v-for="(singer, index) in item.singer">
+            <span v-if="index !== 0">/</span>
+            {{singer.name}}
+          </a>
+          {{getDesc(item)}}
+        </p>
+      </li>
+    </ul>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    name: 'songlist',
+    props: {
+      songlist: {
+        type: Array,
+        default: []
+      }
+    },
+    methods: {
+      getDesc (song) {
+        if (song.desc) {
+          return `-${song.desc}`
+        }
+        return `-${song.albumname}`
+      }
+    }
+  }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  .song-list
+    padding: 10px 30px
+    .list-item
+      line-height: 20px
+      height: 64px
+      .title
+        color: $color-text
+      .desc
+        margin: 4px 0
+        color: $color-text-d
+</style>
+
---
+++ src/components/singer-detail/singer-info.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="singer-info" :style="bgStyle">
+    <div class="wrap">
+      <h2 class="title">
+        <i class="icon-back"></i>
+        {{info.name}}
+      </h2>
+      <div class="content">
+        <span class="desc">{{info.fans}}</span>
+        <div class="play-btn">
+          <i class="icon-play"></i>
+          <span>随机播放全部</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import {normalizeFans} from 'controllers/singer'
+  export default {
+    name: 'singer-info',
+    props: {
+      info: {
+        type: Object,
+        default: {},
+        bgStyle: ''
+      }
+    },
+    created () {
+      this.info.fans = normalizeFans(this.info.fans)
+      this.bgStyle = `background-image:url(${this.info.imgurl})`
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/mixin"
+  @import "~common/stylus/variable"
+  .singer-info
+    position: relative
+    background-size: cover
+    width: 100%
+    height: 0
+    padding-top: 70%
+    .wrap
+      width: 100%
+      position: absolute
+      top: 0
+      bottom: 0
+      background: rgba(7, 17, 27, 0.4)
+      .title
+        padding: 10px
+        font-size: $font-size-large
+        text-align: center
+        line-height: 40px
+        color: $color-text
+        .icon-back
+          color: $color-theme
+          font-size: $font-size-large-x
+          float: left
+          position: relative
+          top: 9px
+      .content
+        position: absolute
+        bottom: 0
+        left: 0
+        text-align: center
+
+
+</style>
+
